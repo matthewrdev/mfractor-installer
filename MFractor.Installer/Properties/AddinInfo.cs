5a0d31f107aaf84790cc5d0eea9354076bf75fae
Matthew Robbins <mrobbins_99@hotmail.com>
2016-08-17 03:16:55 +1000

    Text update, version bump to 2.0.3

--- MFractor.Installer/Properties/AddinInfo.cs
+++ MFractor.Installer/Properties/AddinInfo.cs
@@ -5,11 +5,11 @@ using Mono.Addins.Description;
 [assembly:Addin (
 	"MFractor.Installer", 
 	Namespace = "MFractor.Installer",
-	Version = "2.0.2"
+	Version = "2.0.3"
 )]
 
 [assembly:AddinName ("MFractor.Installer")]
-[assembly:AddinCategory ("IDE Extensions")]
-[assembly:AddinDescription ("Installs MFractor into Xamarin Studio; the essential productivity tool for Xamarin Studio. Featuring Xaml code analysis, Xaml -> C# code generation, Android resource IntelliSense and much, much more.")]
+[assembly:AddinCategory ("IDE extensions")]
+[assembly:AddinDescription ("Installs MFractor into Xamarin Studio.\n\nAs the essential productivity tool for Xamarin Studio, MFractor adds:\n\nXamarin.Forms Xaml code analysis.\nXamarin.Forms Xaml to C# code generation.\nStreamlined Xamarin.Forms navigation.\nIntelliSense for Xamarin.Android Xml Resources.\nStatic analysis for Android Resources\nStreamlined Android resource navigation.\nC# code analysis for mobile apps.")]
 [assembly:AddinUrl("http://www.mfractor.com")]
 [assembly:AddinAuthor ("Matthew Robbins")]
